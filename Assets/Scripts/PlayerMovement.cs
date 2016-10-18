b7260371c7ec19ddaa9b5633ace81680ee1bec12
Hotshot3434 <htst3434@gmail.com>
2016-10-18 11:51:08 -0700

    Worked on movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed = 5;
     public float jumpForce = 5;
-
-    private bool isGrounded = false;
+    public  bool isGrounded = false;
+    
     private Rigidbody2D rb;
 
 	void Start () {
@@ -14,12 +14,24 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	void Update () {
+        //Horizontal Movement
         this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * movementSpeed * Time.deltaTime;
-        
+
+
+        //Jumping
         if (isGrounded)
         {
-            this.transform.position += new Vector3(0, Input.GetAxis("Jump"), 0)*jumpForce*Time.deltaTime;
+            rb.AddForce(new Vector3(0, Input.GetAxis("Jump"), 0) * jumpForce);
             isGrounded = false;
         }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        isGrounded = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
+    }
 }
