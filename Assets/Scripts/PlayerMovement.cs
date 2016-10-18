79b30a9ef1b7a4eebd4b9966b65e49dab367fd37
Hotshot3434 <htst3434@gmail.com>
2016-10-17 21:45:22 -0700

    Cread Repository

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed = 5;
+    public float jumpForce = 5;
+
+    private bool isGrounded = false;
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * movementSpeed * Time.deltaTime;
+        
+        if (isGrounded)
+        {
+            this.transform.position += new Vector3(0, Input.GetAxis("Jump"), 0)*jumpForce*Time.deltaTime;
+            isGrounded = false;
+        }
+	}
+}
