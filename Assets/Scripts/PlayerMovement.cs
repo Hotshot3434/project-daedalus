b861cd9957f8b017816b877e46c201718ef2745a
Hotshot3434 <htst3434@gmail.com>
2016-10-18 15:34:51 -0700

    Tried to fix jumping, narrowed down problem
    The collider placed on the Player game object is not calling the
    OnTriggerEnter method for some reason. Will look into it more

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed = 5;
-    public float jumpForce = 5;
-    public  bool isGrounded = false;
+    public float jumpForce = 200;
+    public bool isGrounded = false;
     
     private Rigidbody2D rb;
 
@@ -19,19 +19,22 @@ public class PlayerMovement : MonoBehaviour {
 
 
         //Jumping
-        if (isGrounded)
+        if (isGrounded && Input.GetAxis("Jump") != 0)
         {
             rb.AddForce(new Vector3(0, Input.GetAxis("Jump"), 0) * jumpForce);
             isGrounded = false;
         }
 	}
 
+    //TODO: Find out why the collider will not trigger these functions
     void OnTriggerEnter(Collider other)
     {
         isGrounded = true;
+        Debug.Log("Trigger Enter");
     }
     void OnTriggerExit(Collider other)
     {
         isGrounded = false;
+        Debug.Log("Trigger Exit");
     }
 }
